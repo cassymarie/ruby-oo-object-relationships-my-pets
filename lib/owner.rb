db935fda1925a7757035dfa8f1ce3dda1d8c55d1
cassymarie <cassy2205@gmail.com>
2020-11-04 21:29:03 -0700

    commit tree cc2d4390dfdf50ca4113d2bcabb246b04f84ff2a

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
+require_relative './cat.rb'
+require_relative './dog.rb'
+
 class Owner
   # code goes here
+  attr_reader :name, :species, :pets
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    self.class.all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count()
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    my_cats = []
+    Cat.all.each{|c| my_cats << c if c.owner == self}
+    my_cats
+  end
+
+  def dogs
+    my_dogs = []
+    Dog.all.each{|d| my_dogs << d if d.owner == self}
+    my_dogs
+  end
+
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
+
+  def buy_cat(new_cat)
+    Cat.new(new_cat, self)
+  end
+
+  def buy_dog(new_dog)
+    Dog.new(new_dog, self)
+  end
+
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood = "happy"}
+  end
+
+
+  def feed_cats
+    self.cats.each{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.each{|cat| 
+      cat.mood = "nervous" 
+      cat.owner = nil
+    }
+    self.dogs.each{|dog| 
+      dog.mood = "nervous" 
+      dog.owner = nil
+    }
+
+  end
+
 end
---
+++ lib/rakefile.rb
@@ -0,0 +1,31 @@
+# require_relative './owner.rb'
+# require_relative './cat.rb'
+# require_relative './dog.rb'
+# # require_relative '../config/environment.rb'
+# require 'pry'
+
+# def reload!
+#   load './owner.rb'
+#   load './cat.rb'
+#   load './dog.rb'
+# end
+
+# victoria = Owner.new("Victoria")
+# beckham = Owner.new("Beckham")
+# Owner.new("Melanie")
+# cassy = Owner.new("Cassy")
+
+# herm = Owner.new("Hermione")
+# crook = Cat.new("Crookshanks", herm)
+# bob = Cat.new("Bobcat",beckham)
+
+# Dog.new("Lassie",beckham)
+# Dog.new("Oreo",cassy)
+# timmy = Owner.new("Timmy")
+
+# cat_1 = Cat.new("Garfield", timmy)
+# cat_2 = Cat.new("Fido", timmy)
+# cat_3 = Cat.new("Whiskers", timmy)
+
+#  binding.pry
+#  crook.name
